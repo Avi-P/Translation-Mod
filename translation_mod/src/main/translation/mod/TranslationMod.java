ea4fe4514a7695e6e3c3fb44306f5c323f4032a0
Avi-P <apooruli@gmail.com>
2018-06-14 22:20:06 -0700

    Set up some starter code and registered event listener

--- translation_mod/src/main/translation/mod/TranslationMod.java
+++ translation_mod/src/main/translation/mod/TranslationMod.java
@@ -1,5 +1,28 @@
 package main.translation.mod;
 
-public class TranslationMod {
+import main.translation.translate.TranslationEventHandler;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
+@Mod(modid = TranslationMod.MODID, name = TranslationMod.NAME, version = TranslationMod.Version)
+public class TranslationMod {
+	
+	public static final String MODID = "TranslationMod";
+	public static final String NAME = "Translation Mod";
+	public static final String Version = "1.0";
+	public static final String acceptedMinecraftVersion = "1.12.2";
+	
+	@EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		MinecraftForge.EVENT_BUS.register(TranslationEventHandler.class);
+		
+        FMLCommonHandler.instance().bus().register(this);
+        //MinecraftForge.EVENT_BUS.register(this);
+    }
+	
+	
 }
---
+++ translation_mod/src/main/translation/translate/TranslationEventHandler.java
@@ -0,0 +1,11 @@
+package main.translation.translate;
+
+import net.minecraft.util.text.event.HoverEvent;
+import net.minecraft.util.text.ITextComponent;
+
+public class TranslationEventHandler {
+	
+	public void translation(HoverEvent hover) {
+		if(hover.getValue() == "")
+	}
+}
