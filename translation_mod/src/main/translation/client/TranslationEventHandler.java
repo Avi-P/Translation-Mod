f67226e777b5ab0ad789a8d954ffc22d5f3c199c
Avi-P <apooruli@gmail.com>
2018-06-21 23:04:30 -0700

    Fixed registering event handler

--- translation_mod/src/main/translation/client/TranslationEventHandler.java
+++ translation_mod/src/main/translation/client/TranslationEventHandler.java
@@ -1,12 +1,13 @@
 package main.translation.client;
 
-import net.minecraft.util.text.event.HoverEvent;
+
+import net.minecraftforge.client.event.MouseEvent;
+import net.minecraftforge.event.entity.player.ArrowNockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
 import org.lwjgl.input.Mouse;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiNewChat;
 import net.minecraft.client.gui.GuiScreen;
 import java.util.List;
 import java.util.ArrayList;
@@ -16,20 +17,23 @@ import main.translation.translate.Translator;
 
 public class TranslationEventHandler {
 	
-	//component.getStyle().getHoverEvent();
-	protected Minecraft mc;
-	Translator http = new Translator();
+	Minecraft mc;
 	GuiScreen screen;
 	
 	@SubscribeEvent
-	public void translation(HoverEvent hover) {
+	public void translation(MouseEvent hover) {
+		
+		//component.getStyle().getHoverEvent();
+		
+		Translator http = new Translator();
+		
 		
 		//itextcomponent instanceof TextComponentString;
 		
-		
+		System.out.println("MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT ");
 		//if(hover.getValue() == this.mc.ingameGUI.getChatGUI().getChatComponent(Mouse.getX(), Mouse.getY())) {}
 		
-		ITextComponent var = hover.getValue();
+		ITextComponent var = this.mc.ingameGUI.getChatGUI().getChatComponent(Mouse.getX(), Mouse.getY());
 				
 		if(var instanceof TextComponentString) {
 			String chatLine = var.getUnformattedComponentText();
--- translation_mod/src/main/translation/client/TranslationMod.java
+++ translation_mod/src/main/translation/client/TranslationMod.java
@@ -1,29 +1,52 @@
 package main.translation.client;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import main.translation.client.TranslationEventHandler;
+import net.minecraft.init.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common;
-import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+;
 
 @Mod(modid = TranslationMod.MODID, name = TranslationMod.NAME, version = TranslationMod.Version)
 public class TranslationMod {
 	
-	public static final String MODID = "TranslationMod";
+	public static final String MODID = "translationmod";
 	public static final String NAME = "Translation Mod";
 	public static final String Version = "1.0";
 	public static final String acceptedMinecraftVersion = "1.12.2";
 	
+	public static final Logger LOGGER = LogManager.getLogger(MODID.toUpperCase());
+	
+	@Mod.Instance(MODID)
+	public static TranslationMod instance;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		
+	}
+	
 	@EventHandler
     public void init(FMLInitializationEvent event)
     {
-		MinecraftForge.EVENT_BUS.register(TranslationEventHandler.class);
 		
-        FMLCommonHandler.instance().bus().register(this);
-        //MinecraftForge.EVENT_BUS.register(this);
+		MinecraftForge.EVENT_BUS.register(new TranslationEventHandler());
+		
+		LOGGER.info("REGISTERED");
+		
     }
 	
+	public static void Postinit(FMLPostInitializationEvent event)
+	{
+		
+	}
 	
 }
